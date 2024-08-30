5750197a751849bc6088018153126eb0e3585b7f
neekit95 <nikita-ribalkin@mail.ru>
2024-08-30 15:48:17 +0300

    STR1: restructured directories, add sass modules, add Redux, update main.tsx

---
+++ src/components/app/app.tsx
@@ -0,0 +1,12 @@
+import style from './app.module.scss';
+
+const App = () => {
+	return (
+		<div className={style.container}>
+            app container
+		</div>
+	);
+};
+
+export default App;
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/app/app';
+import './styles/index.scss';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+	throw new Error('Root element is missing');
+}
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
+);
