2a04f3204485eab797c5b6c57fa661893d1ac4b9
neekit95 <nikita-ribalkin@mail.ru>
2024-08-31 21:01:27 +0300

    LAYOUT2: correct click on burger button, add styles

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,41 @@
 import style from './header.module.scss';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { MdForest } from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdHome } from 'react-icons/io';
+import { PiFinnTheHumanFill } from 'react-icons/pi';
+import { IoSettingsSharp } from 'react-icons/io5';
 
 const Header: React.FC = () => {
-    const [isDropdwnOpen, setIsDropdwnOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
     const toggleDropdown = (): void => {
-        setIsDropdwnOpen(!isDropdwnOpen);
+        console.log('Toggle dropdown');
+        setIsDropdownOpen((prevState) => !prevState);
     };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            // Проверяем, что клик произошел не на кнопке и не в области меню
+            if (
+                dropdownRef.current &&
+                buttonRef.current &&
+                !dropdownRef.current.contains(event.target as Node) &&
+                !buttonRef.current.contains(event.target as Node)
+            ) {
+                setIsDropdownOpen(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
 
     return (
         <div className={style.container}>
@@ -26,35 +52,43 @@ const Header: React.FC = () => {
                         <input type="text" placeholder={'Search'} />
                     </div>
 
-                    <button onClick={toggleDropdown} className={style.navbar}>
+                    <button
+                        ref={buttonRef}
+                        onClick={toggleDropdown}
+                        className={style.navbar}
+                    >
                         <GiHamburgerMenu />
                     </button>
-                    {isDropdwnOpen && (
-                        <div className={style.dropdown}>
+                    {isDropdownOpen && (
+                        <div className={style.dropdown} ref={dropdownRef}>
                             <Link to="/" className={style.dropdownLink}>
                                 <button
                                     onClick={toggleDropdown}
-                                    className={style.droplink}
+                                    className={style.dropbutton}
                                 >
-                                    Домой
+                                    <IoMdHome />
+
+                                    <p>Домой</p>
                                 </button>
                             </Link>
 
                             <Link to="/userpage" className={style.dropdownLink}>
                                 <button
                                     onClick={toggleDropdown}
-                                    className={style.droplink}
+                                    className={style.dropbutton}
                                 >
-                                    Мой профиль
+                                    <PiFinnTheHumanFill />
+                                    <p>Мой профиль</p>
                                 </button>
                             </Link>
 
                             <Link to="/settings" className={style.dropdownLink}>
                                 <button
                                     onClick={toggleDropdown}
-                                    className={style.droplink}
+                                    className={style.dropbutton}
                                 >
-                                    Настройки
+                                    <IoSettingsSharp />
+                                    <p>Настройки</p>
                                 </button>
                             </Link>
                         </div>
