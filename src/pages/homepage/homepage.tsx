f43430155d089c73ac7f636fe137589a389e0b72
neekit95 <nikita-ribalkin@mail.ru>
2024-09-02 20:52:25 +0300

    LAYOUT4: styles to header and homepage, add options

--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Homepage: React.FC = () => {
     return (
         <div className={style.container}>
-            <div className={style.hero}>
+            <section className={style.hero}>
                 <img
                     src={filmwoodImage}
                     alt="filmwood image"
@@ -14,7 +14,11 @@ const Homepage: React.FC = () => {
                 />
 
                 <div className={style.heroContent}>
-                    <h1>Добро пожаловать в FILMWOOD</h1>
+                    <h1>
+                        Добро пожаловать в
+                        <br />
+                        <span>FILMWOOD</span>
+                    </h1>
                     <p>
                         Откройте для себя мир кино, создавайте списки избранных
                         фильмов и делитесь мнением с другими пользователями.
@@ -27,13 +31,42 @@ const Homepage: React.FC = () => {
                         </Link>
                         <Link to="/auth/register">
                             <button className={style.heroButton}>
-                                Загегистрироваться
+                                Зарегистрироваться
                             </button>
                         </Link>
                     </div>
                 </div>
-            </div>
-            {/*<div className="wrapper">wrapper</div>*/}
+            </section>
+            <section className="wrapper">
+                <div className={style.secondSection}>
+                    {/*<h1>Все новинки кино уже здесь!</h1>*/}
+                    <div className={style.options}>
+                        <div className={style.option}>
+                            <h2>Получайте персональные рекомендации</h2>
+                            <p>
+                                Наш алгоритм подберет для вас фильмы и сериалы
+                                на основе ваших предпочтений.
+                            </p>
+                        </div>
+                        <div className={style.option}>
+                            <h2>Не пропускайте выход любимых сериалов</h2>
+                            <p>
+                                Отметьте любимые сериалы и ждите выхода новых
+                                сезонов. Мы уведомим вас, когда новые серии
+                                будут доступны.
+                            </p>
+                        </div>
+                        <div className={style.option}>
+                            <h2>Создавайте и делитесь своими списками</h2>
+                            <p>
+                                Соберите подборки фильмов и сериалов, которые
+                                вам нравятся, и делитесь ими с друзьями. Пусть
+                                ваш вкус вдохновит других!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
