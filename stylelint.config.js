091ef1ca613e0da6fda1a2da7a833a5782a2b6e2
neekit95 <nikita-ribalkin@mail.ru>
2024-09-18 20:32:05 +0300

    DEV7: add rule to stylelint (properties-order), fix orders

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,10 @@
 import stylelintConfigStandard from 'stylelint-config-standard';
 import stylelintScss from 'stylelint-scss';
+import stylelintOrder from 'stylelint-order';
 
 export default {
     extends: [stylelintConfigStandard, stylelintScss],
-    plugins: [stylelintScss],
+    plugins: [stylelintScss, stylelintOrder],
     rules: {
         'selector-pseudo-class-no-unknown': [
             true,
@@ -21,7 +22,54 @@ export default {
             { except: ['first-nested'], ignore: ['after-comment'] },
         ],
         'selector-class-pattern': null,
-        'color-named' : 'never',
-        'color-hex-length': 'long'
+        'color-named': 'never',
+        'color-hex-length': 'long',
+        'order/order': [
+            'custom-properties',
+            'declarations',
+        ],
+        'order/properties-order': [
+            [
+                'position',
+                'top',
+                'right',
+                'bottom',
+                'left',
+                'z-index',
+                'display',
+                'justify-content',
+                'align-items',
+                
+                'width',
+                'height',
+                'min-width',
+                'min-height',
+                'max-width',
+                'max-height',
+                'margin',
+                'padding',
+                'gap',
+                
+                'background',
+                'background-color',
+                'border',
+                'border-radius',
+                'box-shadow',
+                'opacity',
+                
+                'font',
+                'font-size',
+                'font-weight',
+                'line-height',
+                'text-align',
+                'color',
+                
+                'transition',
+                'animation',
+                'z-index'
+            ],
+            { unspecified: 'bottomAlphabetical' },
+        ],
     },
+    fix: true,
 };
